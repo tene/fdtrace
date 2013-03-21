51b4ede432a2f2a89ef3dfbb4c106fcb5e28208a
Stephen Weeks <tene@allalone.org>
2013-03-20 21:29:25 -0700

    Register and call handler functions

--- fdtrace.c
+++ fdtrace.c
@@ -12,11 +12,27 @@
 
 typedef enum { READ, WRITE } iotype_t;
 
-void usage(const char *name) {
-    fprintf(stderr, "Usage: %s PID\n", name);
+typedef void (*iotrace_handler)(pid_t pid, int fd, iotype_t type, size_t len, const char *buf, void *user_data);
+struct iotrace_handler_record {
+    iotrace_handler handler;
+    void *user_data;
+};
+
+static size_t handler_count = 0;
+static size_t handler_buf_size = 0;
+struct iotrace_handler_record *handlers = NULL;
+
+void register_iotrace_handler(iotrace_handler h, void *u) {
+    if (handler_count + 1 >= handler_buf_size) {
+        handler_buf_size = 5 + handler_buf_size * 2;
+        handlers = realloc(handlers, sizeof(struct iotrace_handler_record) * handler_buf_size);
+    }
+    handlers[handler_count].handler = h;
+    handlers[handler_count].user_data = u;
+    handler_count++;
 }
 
-void dump_data(pid_t pid, int fd, iotype_t type, unsigned long int addr, size_t len) {
+static void handle_data(pid_t pid, int fd, iotype_t type, size_t len, unsigned long int addr) {
     char buf[len];
     struct iovec local[1], remote[1];
 
@@ -29,10 +45,13 @@ void dump_data(pid_t pid, int fd, iotype_t type, unsigned long int addr, size_t 
             local, 1,
             remote, 1,
             0);
-    printf("%.*s", (int)len, buf);
+
+    for (size_t i = 0; i < handler_count; i++) {
+        handlers[i].handler(pid, fd, type, len, buf, handlers[i].user_data);
+    }
 }
 
-int handle_syscall(pid_t pid) {
+static int handle_syscall(pid_t pid) {
     static int insyscall = 0;
     static int fd;
     static unsigned long int addr;
@@ -53,7 +72,7 @@ int handle_syscall(pid_t pid) {
             fd = regs.rdi;
             addr = regs.rsi;
             len = regs.rdx;
-            dump_data(pid, fd, WRITE, addr, len);
+            handle_data(pid, fd, WRITE, len, addr);
         } else if (regs.orig_rax == __NR_read) {
             fd = regs.rdi;
             addr = regs.rsi;
@@ -63,7 +82,7 @@ int handle_syscall(pid_t pid) {
         if (regs.orig_rax == __NR_read) {
             len = regs.rax;
             if (len > 0)
-                dump_data(pid, fd, READ, addr, len);
+                handle_data(pid, fd, READ, len, addr);
         }
     }
     return 0;
@@ -116,6 +135,14 @@ int run_trace(const pid_t pid) {
     return 0;
 }
 
+static void usage(const char *name) {
+    fprintf(stderr, "Usage: %s PID\n", name);
+}
+
+void my_handler(pid_t pid, int fd, iotype_t type, size_t len, const char *buf, void *user_data) {
+    printf("%s %zu bytes on fd %d\n", (type == READ) ? "read" : "wrote", len, fd);
+}
+
 int main(int argc, char **argv) {
     if (2 != argc) {
         usage(argv[0]);
@@ -123,6 +150,8 @@ int main(int argc, char **argv) {
     }
     const char *pidstr = argv[1];
     const pid_t pid = atoi(pidstr);
+
+    register_iotrace_handler(my_handler, NULL);
 
     run_trace(pid);
     return 0;
